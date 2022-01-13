f9de4082cabd124f550f1eede79cbca4335fd90b
fuwwyUwU <vsjoqvist@gmail.com>
2022-01-13 21:14:52 +0100

    added website

---
+++ index.js
@@ -0,0 +1,11 @@
+var text = document.querySelector("p");
+var amogus = document.querySelector("img");
+var drippnes = 0;
+
+amogus.onclick = function () {
+  drippnes++;
+  text.innerHTML =
+    drippnes < 69
+      ? `YoU hAVe ${drippnes} <br> SUS`
+      : `you have 69 + ${drippnes - 69} dripness <br> sus king`;
+};
