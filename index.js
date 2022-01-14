532553bd1193993b8898fdd2dbb149fcb20f0824
fuwwyUwU <vsjoqvist@gmail.com>
2022-01-14 16:32:48 +0100

    added coockies

--- index.js
+++ index.js
@@ -1,11 +1,49 @@
 var text = document.querySelector("p");
 var amogus = document.querySelector("img");
-var drippnes = 0;
+var dripness = 0;
+SetUp();
+
+function getCookie(name) {
+  // Add the = sign
+  name = name + "=";
+
+  // Get the decoded cookie
+  const decodedCookie = decodeURIComponent(document.cookie);
+
+  // Get all cookies, split on ; sign
+  const cookies = decodedCookie.split(";");
+
+  // Loop over the cookies
+  for (let i = 0; i < cookies.length; i++) {
+    // Define the single cookie, and remove whitespace
+    const cookie = cookies[i].trim();
+
+    // If this cookie has the name of what we are searching
+    if (cookie.indexOf(name) == 0) {
+      // Return everything after the cookies name
+      return cookie.substring(name.length, cookie.length);
+    }
+  }
+}
+
+function SetUp() {
+  document.cookie = "dripness=1000; expires=Sun, 01 Jan 2100 12:00:00 UTC";
+  console.log(getCookie("dripness"));
+  dripness = getCookie("dripness");
+  text.innerHTML = getScore();
+}
+
+function getScore() {
+  return dripness == 0
+    ? "YoU hAVe 0 DrIPneSs PeAsent"
+    : dripness <= 69
+    ? `YoU hAVe ${dripness} <br> SUS`
+    : dripness <= 489
+    ? `you have 69 + ${dripness - 69} dripness <br> sus king`
+    : `you have 69 + 420 + ${dripness - 489} <br> sus master`;
+}
 
 amogus.onclick = function () {
-  drippnes++;
-  text.innerHTML =
-    drippnes <= 69
-      ? `YoU hAVe ${drippnes} <br> SUS`
-      : `you have 69 + ${drippnes - 69} dripness <br> sus king`;
+  dripness++;
+  text.innerHTML = getScore();
 };
